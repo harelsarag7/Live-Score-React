1aec0e44f70d9dde0d53ba6212404a2ccd87d5b0
harelsarag7 <112413505+harelsarag7@users.noreply.github.com>
2022-11-11 17:43:32 +0200

    organized the code

--- src/Components/Main/LiveSection/LiveSection.tsx
+++ src/Components/Main/LiveSection/LiveSection.tsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import "./LiveSection.css";
-import Card, { LiveScore } from './Card/Card';
+import Card from './Card/Card';
 import CardLastGame from "./CardLastGame/CardLastGame";
 import League from "../League/League";
+import { cardFunctions } from "../../../functions/CardFunctions";
+import { LiveScore } from "../../../interfaces/LiveScoreInterface";
+
 
 const Leagues = [
   {
@@ -50,74 +53,10 @@ const Leagues = [
 function LiveSection(): JSX.Element {
 const [liveGame, SetLiveGame] = useState<LiveScore[]>([])
 const [lastGame, SetLastGame] = useState<LiveScore[]>([])
-const apiKey = "581fb55ff92d00056049811d83a45652c46ae3ff89ec0166c24366d92d25a22c"
-
-async function getApiDataLiveGame() {
-  const response = await fetch(
-    `https://apiv2.allsportsapi.com/football/?met=Livescore&APIkey=${apiKey}`
-  ).then((response) => response.json());
-    
-  return SetLiveGame(response.result? response.result :<h3>No Games</h3>); 
-  }
-async function getApiDataLastGame() {
-  const response = await fetch(
-    `https://apiv2.allsportsapi.com/football/?met=Fixtures&APIkey=${apiKey}&from=2021-05-18&to=2021-05-18`
-  ).then((response) => response.json());
-    
-  return SetLastGame(response.result? response.result :<h3>No Games</h3>); 
-  }
-
-function getYesterdayDate() {
-  let newDate = new Date();
-  let compilerDate = new Date(newDate.getFullYear(), newDate.getMonth(), newDate.getDate()-1);
-
-  let dd = String(compilerDate.getDate()).padStart(2, '0');
-  let mm = String(compilerDate.getMonth() + 1).padStart(2, '0'); //January is 0!
-  let yyyy = compilerDate.getFullYear();
-  
-  let yesterday =  `${yyyy}-${mm}-${dd}`;
-
-console.log(yesterday);
-  return yesterday;
-}
-
-
-function getDateBeforeWeek() {
-    let today = new Date();
-    let compilerDate = new Date(today.getFullYear(), today.getMonth(), today.getDate()-7);
-
-    let dd = String(compilerDate.getDate()).padStart(2, '0');
-  let mm = String(compilerDate.getMonth() + 1).padStart(2, '0');
-  let yyyy = compilerDate.getFullYear();
-  
-  let dateBeforeLastWeek = `${yyyy}-${mm}-${dd}`;
-
-    console.log(dateBeforeLastWeek);
-    return dateBeforeLastWeek;
-}
-
-async function LeagueOnClick(leagueId :number, countryId: number){
-  const liveGames = await fetch(
-    `https://apiv2.allsportsapi.com/football/?met=Livescore&APIkey=${apiKey}}&countryId=${countryId}`
-  ).then((liveGames) => liveGames.json());
-  const filterLiveGames = liveGames.result.filter((team: any) => team.league_key ===  leagueId)
-   SetLiveGame(filterLiveGames); 
-
-
-const yesterday = getYesterdayDate();
-const dateBeforeWeek = getDateBeforeWeek();
-  const response = await fetch(
-    `https://apiv2.allsportsapi.com/football/?met=Fixtures&APIkey=${apiKey}&from=${dateBeforeWeek}&to=${yesterday}&countryId=${countryId}`
-  ).then((response) => response.json())
-  const filterGames = response.result.filter((team: any) => team.league_key ===  leagueId)
-   SetLastGame(filterGames); 
-}
-  
-
 
   useEffect(() => {
-    getApiDataLiveGame()
-    getApiDataLastGame()   
+     cardFunctions.getApiDataLiveGame().then(liveGames => SetLiveGame(liveGames));
+    cardFunctions.getApiDataLastGame().then(lastGames => SetLastGame(lastGames));   
   }, [])
 
   
@@ -125,20 +64,23 @@ const dateBeforeWeek = getDateBeforeWeek();
     return (
         <div className="LiveSection">
           <div className="LeagueDiv">
-                {Leagues.map((league) => <League key={league.id} name={league.name} onclick={() => LeagueOnClick(league.id, league.countryId)} />)}
+                {Leagues.map((league) => <League key={league.id} name={league.name} onclick={() => cardFunctions.LeagueOnClick(league.id, league.countryId).then(res => {
+                  SetLiveGame(res[0])
+                  SetLastGame(res[1])
+                  })}  />)}
             </div>
           <div className="containers">
               <h3 className="games-header">Live Games</h3>
             <div id="live-games-container">
-                {liveGame.length? liveGame.map((item) => {
-                  return <Card key={item.event_key}  game={item} />
-                }): "No live games"} 
+                {liveGame.length? liveGame.map((item) => 
+                   <Card key={item.event_key}  game={item} />
+                ): "No live games"} 
             </div>
               <h3 className="games-header">Last Games</h3>
             <div id="live-games-container-last-game">
-                {lastGame.map((item) => {
-                    return <CardLastGame key={item.event_key} game={item}/>
-                })}
+                {lastGame.map((item) => 
+                     <CardLastGame key={item.event_key} game={item}/>
+                )}
             </div>
           </div>
         </div>
@@ -147,24 +89,3 @@ const dateBeforeWeek = getDateBeforeWeek();
 }
 
 export default LiveSection;
-
-
-
-
-
-
-
-
-
-// // get games by month
-// async function getApiDataLastMonth() {
-//     const response:liveScore = await fetch(
-//       `https://apiv2.allsportsapi.com/football/?met=Fixtures&countryId=62&APIkey=${apiKey}&from=2022-10-01&to=2022-11-06`
-//     ).then((response) => response.json());
-  
-  
-//   // setGames(response)
-//     // card title= Vs= 
-//       return response;
-//   }
-  
---
+++ src/interfaces/LiveScoreInterface.ts
@@ -0,0 +1,97 @@
+// export interface LiveScore {
+    
+// }
+interface Goalscorer {
+   time: string;
+   home_scorer: string;
+   home_scorer_id: string;
+   home_assist: string;
+   home_assist_id: string;
+   score: string;
+   away_scorer: string;
+   away_scorer_id: string;
+   away_assist: string;
+   away_assist_id: string;
+   info: string;
+   info_time: string;
+}
+interface Substitute {
+   time: string;
+   home_scorer: any;
+   home_assist: string;
+   score: string;
+   away_scorer: any;
+   away_assist: string;
+   info: string;
+   info_time: string;
+}
+interface Card {
+   time: string;
+   home_fault: string;
+   card: string;
+   away_fault: string;
+   info: string;
+   home_player_id: string;
+   away_player_id: string;
+   info_time: string;
+}
+interface HomeTeam {
+   starting_lineups: any[];
+   substitutes: any[];
+   coaches: any[];
+   missing_players: any[];
+}
+interface AwayTeam {
+   starting_lineups: any[];
+   substitutes: any[];
+   coaches: any[];
+   missing_players: any[];
+}
+interface Lineups {
+   home_team: HomeTeam;
+   away_team: AwayTeam;
+}
+interface Statistic {
+   type: string;
+   home: string;
+   away: string;
+}
+export interface LiveScore {
+
+   event_key: number;
+   event_date: string;
+   event_time: string;
+   event_home_team?: string;
+   home_team_key: number;
+   event_away_team?: string;
+   away_team_key: number;
+   event_halftime_result: string;
+   event_final_result: string;
+   event_ft_result: string;
+   event_penalty_result: string;
+   event_status: string;
+   country_name: string;
+   league_name: string;
+   league_key: number;
+   league_round: string;
+   league_season: string;
+   event_live: string;
+   event_stadium: string;
+   event_referee: string;
+   home_team_logo: string;
+   away_team_logo: string;
+   event_country_key: number;
+   league_logo: string;
+   country_logo: string;
+   event_home_formation: string;
+   event_away_formation: string;
+   fk_stage_key: number;
+   stage_name: string;
+   league_group?: any;
+   goalscorers: Goalscorer[];
+   substitutes: Substitute[];
+   cards: Card[];
+   lineups: Lineups;
+   statistics: Statistic[];
+   onclick:()=>void
+}
