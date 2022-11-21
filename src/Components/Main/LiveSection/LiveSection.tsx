8e44bb96df82e1238c35d26b7cbf68394f2d143d
ilieBeracha <ilieberacha123@gmail.com>
2022-11-21 17:09:05 +0200

    responsive

--- src/Components/Main/LiveSection/LiveSection.tsx
+++ src/Components/Main/LiveSection/LiveSection.tsx
@@ -21,7 +21,7 @@ function LiveSection(): JSX.Element {
   const [futureGames, SetFututreGames] = useState<LiveScore[] | undefined>([])
   const [localLeague, SetLocalLeague] = useLocalStorage(`league`, `202`)
   const [localCountry, SetLocalCountry] = useLocalStorage(`country`, `62`)
-  const [filteredFutureGames, setFilteredFutureGames] =  useState<string>("");
+  const [filteredFutureGames, setFilteredFutureGames] = useState<string>("");
 
 
   useEffect(() => {
@@ -33,8 +33,8 @@ function LiveSection(): JSX.Element {
     //     }
 
     cardFunctions.LeagueOnClick(Number(localLeague), Number(localCountry)).then(res => {
-        SetLiveGame(res[0])
-        SetLastGame(res[1])
+      SetLiveGame(res[0])
+      SetLastGame(res[1])
     })
 
     cardFunctions.getFuturesGamesByLeague(Number(localLeague), Number(localCountry)).then((res) => {
@@ -44,41 +44,41 @@ function LiveSection(): JSX.Element {
   }, [localLeague, localCountry])
 
   function ClickedLeague(league: number, country: number) {
-    if(league.toString().toLocaleLowerCase() === localLeague){
+    if (league.toString().toLocaleLowerCase() === localLeague) {
       return;
     }
-              SetLiveGame(undefined)
-          SetLastGame(undefined)
-          SetLocalLeague(league.toString().toLocaleLowerCase())
-          SetLocalCountry(country.toString().toLocaleLowerCase())
-}
-  
+    SetLiveGame(undefined)
+    SetLastGame(undefined)
+    SetLocalLeague(league.toString().toLocaleLowerCase())
+    SetLocalCountry(country.toString().toLocaleLowerCase())
+  }
 
-//     function filterBySearch(event)  {
-//   // Access input value
-//   const query = event.target.value;
-//   // Create copy of item list
-//   // var updatedList = [...futureGames];
-//   // Include all elements which includes the search query
-//   const updatedList = futureGames?.filter((item) => {
-//     return 
-//   });
-//   // Trigger render with updated values
-//   setFilteredList(updatedList);
-// };
 
-function filterBySearch(e : any) {
-  console.log(e.target.value);
-  const futureSearchValue = e.target.value;
+  //     function filterBySearch(event)  {
+  //   // Access input value
+  //   const query = event.target.value;
+  //   // Create copy of item list
+  //   // var updatedList = [...futureGames];
+  //   // Include all elements which includes the search query
+  //   const updatedList = futureGames?.filter((item) => {
+  //     return 
+  //   });
+  //   // Trigger render with updated values
+  //   setFilteredList(updatedList);
+  // };
 
-//  const filtered = {futureGames .filter(name => name.includes(futureSearchValue))}
+  function filterBySearch(e: any) {
+    console.log(e.target.value);
+    const futureSearchValue = e.target.value;
 
-  // updatedList = updatedList.filter((item) => {
-  //   return item.toLowerCase().indexOf(query.toLowerCase()) !== -1;
-  // });
-  return setFilteredFutureGames(futureSearchValue.toString())
-  
-}
+    //  const filtered = {futureGames .filter(name => name.includes(futureSearchValue))}
+
+    // updatedList = updatedList.filter((item) => {
+    //   return item.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+    // });
+    // const filteredAfterMap = futureGames?.filter((item)=> item.event_away_team?.includes(futureSearchValue))
+    return setFilteredFutureGames(futureSearchValue.toString())
+  }
 
 
 
@@ -86,65 +86,65 @@ function filterBySearch(e : any) {
     <div className="LiveSection">
       <div className="LeagueDiv">
         {general.Leagues.map((league) => <League key={league.id} image={league.image} name={league.name}
-        onclick={() => ClickedLeague(league.id,league.countryId)}
-           />)}
+          onclick={() => ClickedLeague(league.id, league.countryId)}
+        />)}
       </div>
       <div className="containers">
         <h3 className="games-header">Live Games</h3>
         <div id="live-games-container">
           {liveGame === undefined ?
             <Carousel breakPoints={general.breakPoints}>
-            {loaderCardsArray.loaderCards.map((item) =>
-              <CardLoader key={item.event_key} game={item} />
-            )}
+              {loaderCardsArray.loaderCards.map((item) =>
+                <CardLoader key={item.event_key} game={item} />
+              )}
             </Carousel>
 
-            : liveGame.length === 0 ? 
+            : liveGame.length === 0 ?
               <div className="NoLiveGamesDiv">
                 <p>No Live Games</p>
               </div>
               :
               <Carousel breakPoints={general.breakPoints}>
-              {liveGame.map((item) =>
-                <Card key={item.event_key} game={item} />
-              )}
+                {liveGame.map((item) =>
+                  <Card key={item.event_key} game={item} />
+                )}
               </Carousel>
-              }
+          }
         </div>
 
         <h3 className="games-header">Last Games</h3>
         <div id="live-games-container-last-game">
           {lastGame === undefined ?
             <Carousel breakPoints={general.breakPoints}>
-            {loaderCardsArray.loaderCards.map((item) =>
-              <CardLastGameLoader key={item.event_key} game={item} />
+              {loaderCardsArray.loaderCards.map((item) =>
+                <CardLastGameLoader key={item.event_key} game={item} />
               )}
             </Carousel>
             // "loading.."
-          
-              : lastGame.length === 0 ? 'No Last Games' :
+
+            : lastGame.length === 0 ? 'No Last Games' :
 
               <Carousel breakPoints={general.breakPoints}>
-              {lastGame.map((item) => (
-                <CardLastGame key={item.event_key} game={item} />
+                {lastGame.map((item) => (
+                  <CardLastGame key={item.event_key} game={item} />
                 ))}
-             </Carousel>
-}
+              </Carousel>
+          }
         </div>
 
 
         <div className="futureGamesAndVideo">
-              <div className="future-games">
-                <p>Future Games:</p>
-                <input type="text" onChange={(e) => filterBySearch(e)} placeholder="Search team" name="" id="search-future-games" />
-              {futureGames?.length === 0 ? "Loading...":
-              futureGames === undefined ? "No Future Games":
-              // {names.filter(name => name.includes('J')) 
-              futureGames.filter(team => team.event_home_team?.toLocaleLowerCase().includes(filteredFutureGames)).map((item) => (
-                <CardFuture key={item.event_key} game={item} />
+          <div className="future-games">
+            <p>Future Games:</p>
+            <input type="text" onChange={(e) => filterBySearch(e)} placeholder="Search team" name="" id="search-future-games" />
+            {futureGames?.length === 0 ? "Loading..." :
+              futureGames === undefined ? "No Future Games" :
+                // {names.filter(name => name.includes('J')) 
+                futureGames.filter(team => team.event_away_team?.toLocaleLowerCase().includes(filteredFutureGames) || team.event_home_team?.toLocaleLowerCase().includes(filteredFutureGames)).map((item) => (
+                  <CardFuture key={item.event_key} game={item} />
                 ))}
-              </div>
-              <iframe id="video-section"  src="https://www.youtube.com/embed/BHQCqOpo0nk?autoplay=1&mute=1" allowFullScreen  title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+          </div>
+          <iframe id="video-section" src="https://www.youtube.com/embed/BHQCqOpo0nk?autoplay=1&mute=1" allowFullScreen title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
         </div>
       </div>
     </div>
---
+++ src/Components/Main/LiveSection/PopUp/PopUp.tsx
@@ -0,0 +1,14 @@
+import "./PopUp.css";
+
+function PopUp(): JSX.Element {
+    return (
+        <div className="PopUp-box">
+			 <div className="box">
+                <button className="btn-close">X</button>
+                pop up Component
+             </div>
+        </div>
+    );
+}
+
+export default PopUp;
